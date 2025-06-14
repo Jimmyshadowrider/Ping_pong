085484134b1e8c4c932641dd6b9d82dca1ddc181
Jimmyshadowrider <cud61114892@gmail.com>
2025-06-14 11:42:26 +0700

    Add ball movement ,collisions, and losing conditions
    ball moves randomly. 
    If collide with top or bottom wall, it will bounce off in the opposite direction in y axis.
    If collide with racket, it will bounce off in the opposite direction in x axis.
    If collide with side walls, it will trigger a losing screen (Text depending on which side it collided with).

--- Ping_pong.py
+++ Ping_pong.py
@@ -1,6 +1,6 @@
 
 from pygame import *
-
+from time import sleep
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y,size_x, size_y, player_speed):
         sprite.Sprite.__init__(self)
@@ -29,7 +29,7 @@ class Player(GameSprite):
         if key_pressed[K_DOWN] and self.rect.x < w_width-80:
             self.rect.y += self.speed
 
-
+ball = GameSprite('ball_pingpong.png',200,200,50,50,4)
 racket_l = Player('racket.png',30,200,100,150,4)
 racket_r = Player('racket.png',470,200,100,150,4)
 
@@ -39,7 +39,16 @@ w_height = 500
 window = display.set_mode((w_width,w_height))
 window.fill(back)
 
+font.init()
+font =font.Font(None,35)
+lose1 = font.render('Player 1 lose',True,(180,0,0))
+lose2 = font.render('Player 2 lose',True,(180,0,0))
+
+speed_x = 3
+speed_y = 3
+
 run = True
+finish = False
 clock = time.Clock()
 FPS = 60
 while run:
@@ -47,11 +56,35 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-    window.fill(back)
-    racket_l.update_l()
-    racket_r.update_r()
+    if finish == False:
+        window.fill(back)
+    
+        racket_l.update_l()
+        racket_r.update_r()
+    
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(ball, racket_l) or sprite.collide_rect(ball, racket_r):
+            speed_x *= -1
+            speed_y *= 1
+        if ball.rect.y < 0 or ball.rect.y > w_height-50:
+           speed_y *= -1
+        if ball.rect.x < 0:
+            window.blit(lose1,(200,200))
+            finish = True
+            sleep(1)
+            
+        if ball.rect.x > w_width:
+            window.blit(lose2,(200,200))
+            finish = True
+            sleep(1)
+            
+        
+
+
     racket_l.reset()
     racket_r.reset()
+    ball.reset()
 
 
     display.update()
